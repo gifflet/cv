6454610fa8c7ad2387c1de4ef1c30c28f9376d61
gifflet <guisousa09@hotmail.com>
2024-10-15 21:21:34 -0300

    chore: initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showContact() {
+    var contactInfo = document.getElementById("contact");
+    if (contactInfo.style.display === "none") {
+        contactInfo.style.display = "block";
+    } else {
+        contactInfo.style.display = "none";
+    }
+}
